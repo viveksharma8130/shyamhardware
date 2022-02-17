fd31924b836829f0ccc53fdbfee09c4e8595d533
Vivek Sharma <32999354+viveksharma8130@users.noreply.github.com>
2022-02-17 16:54:37 +0530

    ads
    sds

--- src/utils/firebase/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "swmg-36aa8.appspot.com/images",  // gs://swmg-36aa8.appspot.com/images
-})
-
-export default admin;
